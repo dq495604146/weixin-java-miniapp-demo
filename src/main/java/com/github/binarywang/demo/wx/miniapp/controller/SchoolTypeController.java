f506e556e25d80bc584b0d306416addc38b63610
dingqiang <lbx23059@sina.com>
2023-06-16 20:56:12 +0800

    1.查询省份。2.修改设置个人资料。3.查询所有批次，4.查询所有学校类型。5.查询学校特色

--- src/main/java/com/github/binarywang/demo/wx/miniapp/controller/SchoolTypeController.java
+++ src/main/java/com/github/binarywang/demo/wx/miniapp/controller/SchoolTypeController.java
@@ -1,9 +1,19 @@
 package com.github.binarywang.demo.wx.miniapp.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.github.binarywang.demo.wx.miniapp.dto.ResponseDTO;
+import com.github.binarywang.demo.wx.miniapp.entity.Province;
+import com.github.binarywang.demo.wx.miniapp.entity.SchoolType;
+import com.github.binarywang.demo.wx.miniapp.service.IProvinceService;
+import com.github.binarywang.demo.wx.miniapp.service.ISchoolTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +27,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/schoolType")
 public class SchoolTypeController {
 
+    @Autowired
+    private ISchoolTypeService schoolTypeService;
+    /**
+     * 获取所有学校类型
+     * @return
+     */
+    @GetMapping("/getAllSchoolType")
+    public ResponseDTO<List<SchoolType>> getAllSchoolType(){
+        QueryWrapper qw=new QueryWrapper();
+        qw.orderByAsc("school_type_id");
+        List<SchoolType> list=schoolTypeService.list(qw);
+        return ResponseDTO.success(list);
+    }
 }
 
--- src/main/java/com/github/binarywang/demo/wx/miniapp/service/impl/TPersonalDataServiceImpl.java
+++ src/main/java/com/github/binarywang/demo/wx/miniapp/service/impl/TPersonalDataServiceImpl.java
@@ -1,10 +1,12 @@
 package com.github.binarywang.demo.wx.miniapp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.binarywang.demo.wx.miniapp.entity.TPersonalData;
 import com.github.binarywang.demo.wx.miniapp.dao.TPersonalDataDao;
 import com.github.binarywang.demo.wx.miniapp.service.ITPersonalDataService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * <p>
@@ -17,4 +19,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class TPersonalDataServiceImpl extends ServiceImpl<TPersonalDataDao, TPersonalData> implements ITPersonalDataService {
 
+    @Transactional
+    @Override
+    public TPersonalData saveTPersonalData(TPersonalData tPersonalData) {
+        QueryWrapper qw=new QueryWrapper();
+        qw.eq("open_id",tPersonalData.getOpenId());
+        TPersonalData old=this.getOne(qw);
+        if(old==null){
+            this.save(tPersonalData);
+        }else{
+            tPersonalData.setId(old.getId());
+            this.updateById(old);
+        }
+        return tPersonalData;
+    }
 }
